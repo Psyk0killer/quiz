c230b6c52fb99916ed2263923bfa298367e5d196
tomas.cortes <tomas.cortes@gmail.com>
2015-06-27 18:45:19 +0200

    Ejercicio obligatorio modulo 6

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -13,3 +13,8 @@ exports.answer = function(req, res){
         res.render('quizes/answer', {respuesta: 'Incorrecto'});
     }
 }
+
+// GET /author
+exports.author = function(req,res){
+    res.render('quizes/author');
+}
