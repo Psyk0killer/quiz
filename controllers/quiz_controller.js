05ca981b9cd3f87938f93e1d94a4db29e1e9f27f
tomas.cortes <tomas.cortes@gmail.com>
2015-07-04 12:22:32 +0200

    autoload

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,17 +3,32 @@
 //importamos el modelo 
 var models = require('../models/models.js');
 
+
+// Autoload - factoriza el código si la ruta incluye :quizId
+exports.load = function(req, res, next, quizId){
+    models.Quiz.findById(quizId).then(
+        function(quiz){
+            if(quiz){
+                req.quiz = quiz;
+                next();
+            }else{
+                next(new Error('No existe quizId='+ quizId));
+            }
+        }
+    ).catch(function(error){ next(error); });
+};
+
 // GET /quizes
 exports.index = function(req, res){
     models.Quiz.findAll().then(function(quizes){
         res.render('quizes/index.ejs', { quizes: quizes });
-    });
+    }).catch(function(error){ next(error); });
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-    models.Quiz.find(req.params.quizId).then(function(quiz){
-        res.render('quizes/show', { quiz: quiz});
+    models.Quiz.findById(req.params.quizId).then(function(quiz){
+        res.render('quizes/show', { quiz: req.quiz});
     });
     /*
     models.Quiz.findAll().then(function(quiz) {
@@ -26,16 +41,12 @@ exports.show = function(req, res) {
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-    models.Quiz.find(req.params.quizId).then(function(quiz){
-        if (req.query.respuesta === quiz.respuesta) {
-            res.render('quizes/answer', {
-                quiz: quiz, respuesta: 'Correcto'
-            });
-        } else {
-            res.render('quizes/answer', {
-                quiz:quiz, respuesta: 'Incorrecto'
-            });
+    models.Quiz.findById(req.params.quizId).then(function(quiz){
+        var resultado = 'Incorrecto';
+        if (req.query.respuesta === req.quiz.respuesta) {
+            resultado = 'Correcto';
         }
+        res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
     });
     /*
     models.Quiz.findAll().then(function(quiz) {
